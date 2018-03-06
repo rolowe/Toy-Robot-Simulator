9c18d39ca8f5f25563a1ca62d6c317f5c63758fe
Robert Lowe <robertlowe15@gmail.com>
2018-03-06 00:51:47 +0000

    All commands added

--- assets/js/robot.js
+++ assets/js/robot.js
@@ -6,21 +6,29 @@
       },
       methods: {
           runCommand(){
-            var commandArray = commandInput.value.split('\n');
-
-            //Place array
-            var placeArray = commandArray[0].split(/[ ,]+/);
-            reportRobot(ToyRobot);
-            placeRobot(placeArray);
+            commandArray = commandInput.value.split('\n');
             
+            commandArray.forEach(function(commandLine) {
+                if (commandLine.includes('PLACE')) {
+                  placeRobot(commandLine);
+                }
+                if (commandLine == 'MOVE') {
+                  moveRobot(ToyRobot);
+                }
+                if (commandLine == 'LEFT' || commandLine == 'RIGHT') {
+                  robotDirection(ToyRobot, commandLine)
+                }
+                if (commandLine == 'REPORT') {
+                  reportRobot(ToyRobot);
+                }
+            });
           }
       }
     });
 
 
-
-    /** Defining the Table size - 0,1,2,3,4 **/
-    var tableSize = {x: 4, y: 4};
+    /** Defining the Table size **/
+    var tableSize = {x: 5, y: 5};
 
     /** Robot state **/
     var ToyRobot = {
@@ -33,25 +41,29 @@
     };
 
 
-    function placeRobot(placeArray) {
-      var placeCommand = placeArray[0], placeX = placeArray[1], placeY = placeArray[2], placeDirection = placeArray[3];
+    /************************************
+    ************* Place Robot ***********
+    ************************************/
+
+    function placeRobot(commandLine) {
+      commandLine = commandLine.split(/[ ,]+/);
+      placeCommand = commandLine[0], placeX = commandLine[1], placeY = commandLine[2], placeDirection = commandLine[3];
 
       if (placeCommand == "PLACE") {
-        ToyRobot.placed = true;
 
         if (placeX >= 0 && placeX <= tableSize.x) {
           ToyRobot.position.x = placeX;
+          ToyRobot.placed = true;
         } else {
           return false
         }
         if (placeY >= 0 && placeY <= tableSize.y) {
           ToyRobot.position.y = placeY;
+          ToyRobot.placed = true;
         } else {
           return false
         }
         ToyRobot.direction = placeDirection;
-        
-        console.log(ToyRobot);
       }
       else {
         alert("Failure! First command must be to place robot on table");
@@ -59,45 +71,109 @@
     }
 
 
-    
+    /************************************
+    ** Move robot in current direction **
+    ************************************/
 
+    function moveRobot(ToyRobot) {
+      
+      // Terminate if Robot is not placed
+      if (ToyRobot.placed == false) {
+        return false;
+      }
+
+      var x = ToyRobot.position.x;
+      var y = ToyRobot.position.y;
+
+      switch (ToyRobot.direction) {
+        case 'NORTH':
+          if (++y < tableSize.y) {
+            ToyRobot.position = {x: x, y: y}
+          }
+          break;
+        case 'EAST':
+          if (++x < tableSize.x) {
+            ToyRobot.position = {x: x, y: y}
+          }
+          break;
+        case 'SOUTH':
+          if (--y >= 0) {
+            ToyRobot.position = {x: x, y: y};
+          }
+          break;
+        case 'WEST':
+          if (--x >= 0) {
+            ToyRobot.position = {x: x, y: y}
+          }
+          break;
+        default:
+          break;
+      }
+    };
+
+
+    /*********************************
+    **** Map for turning the robot ***
+    *********************************/
+    var directionMap = {
+      NORTH: {
+        LEFT: 'WEST',
+        RIGHT: 'EAST'
+      },
+      EAST: {
+        LEFT: 'NORTH',
+        RIGHT: 'SOUTH'
+      },
+      SOUTH: {
+        LEFT: 'EAST',
+        RIGHT: 'WEST'
+      },
+      WEST: {
+        LEFT: 'SOUTH',
+        RIGHT: 'NORTH'
+      }
+    };
+
+
+    /*********************************
+    **** Turn the robot and update ***
+    *********************************/
+    function robotDirection(ToyRobot, commandLine) {
+
+      // Terminate if Robot is not placed
+      if (ToyRobot.placed == false) {
+        return false;
+      }
+
+      currentDirection = ToyRobot.direction;
+      newDirection = directionMap[currentDirection][commandLine];
+      ToyRobot.direction = newDirection
+    };
+
+
+    /*********************************
+    **** Report on robot position  ***
+    *********************************/
     function reportRobot(ToyRobot) {
+        // Terminate if Robot is not placed
+        if (ToyRobot.placed == false) {
+          return false;
+        }
+
+        endXPos = ToyRobot.position.x;
+        endYPos = ToyRobot.position.y;
+        endDirection = ToyRobot.direction;
+
+        console.log(ToyRobot);
 
         var app2 = new Vue({
           el: '#report',
           data: {
-            report: 'Hello Vue!'
+            report: 'Output: ' + endXPos + ',' + endYPos + ',' + endDirection
           }
         })
-
     }  
 
-  
 
 
 
-    /** Map for turning the robot **/
-    var directionMap = {
-      north: {
-        value: 'north',
-        left: 'west',
-        right: 'east'
-      },
-      east: {
-        value: 'east',
-        left: 'north',
-        right: 'south'
-      },
-      south: {
-        value: 'south',
-        left: 'east',
-        right: 'west'
-      },
-      west: {
-        value: 'west',
-        left: 'south',
-        right: 'north'
-      }
-    };
-
-    console.log(directionMap);
